e25a93e96168356aaa38eedd2eb54f6944c753b0
Alvi Ahmed <alviahmed811@gmail.com>
2019-11-19 15:47:09 +0600

    called air quality api and brought results

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
+import moment from 'moment'
 
 const styles = {
   app: {
@@ -10,10 +12,36 @@ const styles = {
 }
 
 class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      base_url : 'https://api.openaq.org/v1/',
+      date: moment().format('YYYY-M-DD')
+    }
+  }
+
+  componentDidMount()
+  {
+    let data = {
+      'coordinates': "23.7197845,90.4211198",
+      'radius' : 10000,
+      'date_from': this.state.date,
+      'date_to': this.state.date
+    }
+    axios.get(this.state.base_url+'measurements', {params: data}).then(response => {
+      console.log(response.data.results);
+  }).catch(function (error) {
+      console.log(error);
+  });
+}
+  
+
   render() {
     return (
       <div style={styles.app}>
-        Welcome to React!
+        Welcome to AirChecker!
       </div>
     )
   }
